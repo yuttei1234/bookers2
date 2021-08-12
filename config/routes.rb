ea7d82f989f73c364d70aeac5a50d3967eeeb8c4
yuttei1234 <yuchin1234@gmail.com>
2021-08-12 01:51:07 +0000

    [Update]5-10 ファイルの設定まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+
 end
