4b570f75b06cd0678b197ab84e34581a0b09020f
yuttei1234 <yuchin1234@gmail.com>
2021-08-21 10:28:34 +0000

    [Revise]5-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,13 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
     @book = Book.new
     @books = Book.find(params[:id])
+    @user = current_user
   end
 
   def create
