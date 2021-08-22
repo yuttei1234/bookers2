8a0d4c9c7db0652917923819060c55671efbb6a9
yuttei1234 <yuchin1234@gmail.com>
2021-08-22 05:39:49 +0000

    [5-10]ユーザーの編集URLコート修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  params.require(:name).permit(:name, :introduction, :profile_image)
+  params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
---
+++ db/migrate/20210822051442_add_image_id_to_book.rb
@@ -0,0 +1,5 @@
+class AddImageIdToBook < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :image_id, :string
+  end
+end
